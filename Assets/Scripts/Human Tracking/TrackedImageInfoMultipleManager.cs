0aa2a995d6fcf3cfb3895ce2379cd4d502bda121
NatiPerez459 <natalia.roldan@upr.edu>
2022-04-28 11:22:38 -0400

    simplified and commented image tracker code
    simplified and commented image tracker code

--- Assets/Scripts/Human Tracking/TrackedImageInfoMultipleManager.cs
+++ Assets/Scripts/Human Tracking/TrackedImageInfoMultipleManager.cs
@@ -7,29 +7,31 @@ using UnityEngine.XR.ARFoundation;
 public class TrackedImageInfoMultipleManager : MonoBehaviour
 {
 
+    //object that will be instantiated
     [SerializeField]
-    private GameObject[] arObjectsToPlace;
+    private GameObject arObjectToPlace;
 
+    //the scale that the object will be set to
     [SerializeField]
     private Vector3 scaleFactor = new Vector3(0.1f,0.1f,0.1f);
 
+    //reference to the image tracker manager from AR Foundation
     private ARTrackedImageManager m_TrackedImageManager;
 
+    //dictionary that will assosiate the objects with their names
     private Dictionary<string, GameObject> arObjects = new Dictionary<string, GameObject>();
 
+    //the awake function creates a new object of the instantiated object to place and adds the object to the dictionary
     void Awake()
     {
         m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
         
-        // setup all game objects in dictionary
-        foreach(GameObject arObject in arObjectsToPlace)
-        {
-            GameObject newARObject = Instantiate(arObject, Vector3.zero, Quaternion.identity);
-            newARObject.name = arObject.name;
-            arObjects.Add(arObject.name, newARObject);
-        }
+        GameObject newARObject = Instantiate(arObjectToPlace, Vector3.zero, Quaternion.identity);
+        newARObject.name = arObjectToPlace.name;
+        arObjects.Add(arObjectToPlace.name, newARObject);
     }
 
+    // begins and ends the image tracking
     void OnEnable()
     {
         m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
@@ -40,6 +42,7 @@ public class TrackedImageInfoMultipleManager : MonoBehaviour
         m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
     }
 
+    //when the image is found it will update its information calling the UpdateARImage method
     void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (ARTrackedImage trackedImage in eventArgs.added)
@@ -48,31 +51,23 @@ public class TrackedImageInfoMultipleManager : MonoBehaviour
         }
     }
 
+    //method that will create the object in the AR plane
     private void UpdateARImage(ARTrackedImage trackedImage)
     {
-
         // Assign and Place Game Object
         AssignGameObject(trackedImage.referenceImage.name, trackedImage.transform.position);
-
-        Debug.Log($"trackedImage.referenceImage.name: {trackedImage.referenceImage.name}");
     }
 
+    //Assigns and places the object
     void AssignGameObject(string name, Vector3 newPosition)
     {
-        if(arObjectsToPlace != null)
+        //if the object is not empty it places it on the corresponding location
+        if(arObjectToPlace != null)
         {
             GameObject goARObject = arObjects[name];
             goARObject.SetActive(true);
             goARObject.transform.position = newPosition;
             goARObject.transform.localScale = scaleFactor;
-            foreach(GameObject go in arObjects.Values)
-            {
-                Debug.Log($"Go in arObjects.Values: {go.name}");
-                if(go.name != name)
-                {
-                    go.SetActive(false);
-                }
-            } 
         }
     }
 }
